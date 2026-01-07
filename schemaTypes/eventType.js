fd1389afe066d3a40a31f163e1ce96196ec26f3d
Janefur <annika@hajagency.com>
2026-01-07 13:40:36 +0100

    initial commit

---
+++ schemaTypes/eventType.js
@@ -0,0 +1,37 @@
+export const eventType = {
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Event Name',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      title: 'Event Date',
+      type: 'datetime',
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'photo',
+      title: 'Event Photo',
+      type: 'image',
+    },
+    {
+      name: 'numberOfAttendees',
+      title: 'Number of Attendees',
+      type: 'number',
+    },
+  ],
+}
