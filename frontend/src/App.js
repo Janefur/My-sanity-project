c08e4da03cce718b98375bf0869c7517a12ba056
Therese Lagerhäll <157685965+Thela1989@users.noreply.github.com>
2026-01-08 11:41:56 +0100

    Added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,8 @@
-import React, { useEffect, useState } from "react";
-
-import { sanityClient } from "./sanityClient"; // <-- här importerar du klienten
+import StartPage from "./pages/StartPage";
+//import SingleEvent from "./pages/SingleEvent";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      // Expandera bildreferensen för att få URL:en
-      const query = `*[_type == "event"] {
-        _id,
-        name,
-        date,
-        location,
-        description,
-        numberOfAttendees,
-        tags,
-        "imageUrl": photo.asset->url
-      }`;
-
-      const data = await sanityClient.fetch(query);
-
-      setPosts(data);
-    }
-
-    fetchPosts();
-  }, []);
-
-  return (
-    <div>
-      <h1>Events från Sanity</h1>
-
-      {posts.map((post) => (
-        <div key={post._id}>
-          <h2>{post.name}</h2>
-          <p>📍 {post.location}</p>
-          <p>📅 {new Date(post.date).toLocaleDateString()}</p>
-          {post.description && <p>{post.description}</p>}
-          {post.tags && post.tags.length > 0 && (
-            <div style={{ marginBottom: '10px' }}>
-              {post.tags.map((tag, index) => (
-                <span
-                  key={index}
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-          )}
-          {post.imageUrl && (
-            <img
-              src={post.imageUrl}
-              alt={post.name}
-              style={{ maxWidth: '400px', height: 'auto' }}
-            />
-          )}
-        </div>
-      ))}
-    </div>
-  );
+  return <StartPage />;
 }
 
 export default App;
---
+++ frontend/src/pages/SingleEvent.jsx
@@ -0,0 +1,2 @@
+function SingleEvent() {}
+export default SingleEvent;
